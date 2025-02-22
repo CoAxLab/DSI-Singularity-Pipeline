11d5ff699faccf27fd41cafb1d64ae3f14228464
PETER K <kasatyr@gmail.com>
2025-02-21 23:26:02 -0500

    ignores BIDS metadata

--- runQSIPrep.py
+++ runQSIPrep.py
@@ -10,15 +10,16 @@ sifDirectory = os.path.join(pipelineDirectory, 'SingularitySIFs')
 sourceDirectory = os.path.join(pipelineDirectory, 'bids')
 outputDirectoryQSI = os.path.join(pipelineDirectory, 'qsiprep')
 fsLicense = os.path.join(pipelineDirectory, 'license.txt')
-sifFile = os.path.join(sifDirectory, 'qsiprep_latest.sif')
+sifFile = os.path.join(sifDirectory, 'qsiprep_1.0.0.sif')
 
-singularityCommandPart = f'singularity exec {sifFile}'
+singularityCommandPart = f'singularity run {sifFile}'
 
 def dsiPrint(text):
     print(f'\nDSI-PIPELINE: {text}\n')
 
 ## run src on BIDS folder for each subject
 for subjID in os.listdir(sourceDirectory):
+    if '.tsv' in subjID or '.json' in subjID: continue
     # source files:
     bidsPath = os.path.join(sourceDirectory, subjID)
     # output files:
@@ -31,7 +32,7 @@ for subjID in os.listdir(sourceDirectory):
         dsiPrint(f'QSIPrep already complete for subject: {subjID}! Attempting to continue pipeline...')
         continue
     options = f'--participant_label {subjID} --output-resolution 2 --unringing-method mrdegibbs --fs-license-file {fsLicense}'
-    qsiCommandPart = f'qsiprep {bidsPath} {subjOutDir} -w {workDir} {options}'
+    qsiCommandPart = f'{bidsPath} {subjOutDir} -w {workDir} {options}'
 
     fullCommandQSI = f'{singularityCommandPart} {qsiCommandPart}'
     dsiPrint(f'Running QSIPrep for subject: {subjID}.....')
