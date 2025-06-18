f5db8eb09a953fcbecb227127fd682d89d48771c
Peter <kasatyr@gmail.com>
2025-06-18 00:45:13 -0400

    updated var naems to docker

--- runQSIPrep.py
+++ runQSIPrep.py
@@ -6,13 +6,11 @@ Note: os.system() uses C command: system()
 import os
 
 pipelineDirectory = os.getcwd()
-sifDirectory = os.path.join(pipelineDirectory, 'SingularitySIFs')
 sourceDirectory = os.path.join(pipelineDirectory, 'bids')
 outputDirectoryQSI = os.path.join(pipelineDirectory, 'qsiprep')
 fsLicense = os.path.join(pipelineDirectory, 'license.txt')
-sifFile = os.path.join(sifDirectory, 'qsiprep_latest.sif')
 
-singularityCommandPart = f'docker run -it --rm -v "{sourceDirectory}":/bids -v "{outputDirectoryQSI}":/output pennlinc/qsiprep:latest'
+dockerCommandPart = f'docker run -it --rm -v "{sourceDirectory}":/bids -v "{outputDirectoryQSI}":/output pennlinc/qsiprep:latest'
 
 def dsiPrint(text):
     print(f'\nDSI-PIPELINE: {text}\n')
@@ -32,7 +30,7 @@ for subjID in os.listdir(sourceDirectory):
     options = f'--participant-label {subjID} --output-resolution 2 --unringing-method mrdegibbs --omp-nthreads 8 --nthreads 8 --mem-mb 16000 --fs-license-file "{fsLicense}"'
     qsiCommandPart = f'/bids /output  participant --output-resolution 2 -w /output/work --skip-bids-validation {options}'
 
-    fullCommandQSI = f'{singularityCommandPart} {qsiCommandPart}'
+    fullCommandQSI = f'{dockerCommandPart} {qsiCommandPart}'
     dsiPrint(f'Running QSIPrep for subject: {subjID}.....')
     dsiPrint(fullCommandQSI)
     os.system(fullCommandQSI)
